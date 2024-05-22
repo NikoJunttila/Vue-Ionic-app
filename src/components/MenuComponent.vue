49700cdb77dc84e6d7d3c18b7c7bf428c7fd8ba8
NikoJunttila <89527972+NikoJunttila@users.noreply.github.com>
2024-05-22 19:47:15 +0300

    authentication

--- src/components/MenuComponent.vue
+++ src/components/MenuComponent.vue
@@ -5,10 +5,15 @@
         <ion-title>Side bar</ion-title>
       </ion-toolbar>
     </ion-header>
-    <ion-content class="ion-padding">Login to get access to zyzz bot and music
-      <br>
-      <IonButton expand="full" @click="modalChange">Login</IonButton>
-
+    <ion-content class="ion-padding">
+      <div v-if="!isLoggedIn">
+        Login to get access to zyzz bot and music
+        <br>
+        <IonButton expand="full" @click="modalChange">Login</IonButton>
+      </div>
+      <div v-else>
+        zyzz bot activated soon
+      </div>
     </ion-content>
   </ion-menu>
   <ion-page id="main-content">
@@ -22,7 +27,7 @@
     </ion-header>
     <ion-content class="ion-padding">
       <slot></slot>
-      <ModalLogin :isOpen="isOpen" @some-event="modalChange"></ModalLogin>
+      <ModalLogin :isOpen="isOpen" @some-event="modalChange" :isLoggedIn="isLoggedIn"></ModalLogin>
     </ion-content>
   </ion-page>
 </template>
@@ -40,11 +45,23 @@ import {
   IonButton
 } from "@ionic/vue";
 import ModalLogin from "./ModalLogin.vue";
-/* import { defineComponent } from "vue"; */
-import { ref } from "vue";
+import {onAuthStateChanged} from "firebase/auth"
+import {auth} from "../utils/firebase"
+import { ref, onMounted } from "vue";
 const isOpen = ref<Boolean>(false);
+const isLoggedIn = ref(false)
 function modalChange() {
   console.log(isOpen.value)
   isOpen.value = !isOpen.value;
 }
+onMounted(()=> {
+    onAuthStateChanged(auth, (user) => {
+        console.log(user)
+        if (user){
+            isLoggedIn.value = true
+        } else {
+            isLoggedIn.value = false
+        }
+    })
+})
 </script>
--- src/components/ModalLogin.vue
+++ src/components/ModalLogin.vue
@@ -1,21 +1,48 @@
 <template>
-    <ion-modal :is-open>
-      <ion-header>
-        <ion-toolbar>
-          <ion-buttons slot="start">
-            <ion-button @click="$emit('someEvent')"><ion-icon :icon="arrowBackCircleOutline"></ion-icon></ion-button>
-          </ion-buttons>
-          <ion-title>Log in</ion-title>
-        </ion-toolbar>
-      </ion-header>
-      <ion-content class="ion-padding" color="primary">
-       <slot></slot>
-      </ion-content>
-    </ion-modal>
+  <ion-modal :is-open>
+    <ion-header>
+      <ion-toolbar>
+        <ion-buttons slot="start">
+          <ion-button @click="$emit('someEvent')"
+            ><ion-icon :icon="arrowBackCircleOutline"></ion-icon
+          ></ion-button>
+        </ion-buttons>
+        <ion-title>Log in</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content class="ion-padding" color="primary">
+        <div v-if="!isLoggedIn">
+            <p>
+                login/sign in. if account hasnt been created this will create a new
+                account for you
+            </p>
+            <ion-list>
+                <ion-item>
+                    <ion-input v-model="text" label="Name/Email" label-placement="floating" placeholder="Enter text"></ion-input>
+                </ion-item>
+                <ion-item>
+                    <ion-input
+                    v-model="pass"
+                    label="Password"
+                    label-placement="floating"
+                    type="password"
+                    value="password"
+                    minlength="6"
+                    ></ion-input>
+                </ion-item>
+                <div class="grid-2col">
+                    <ion-button @click="login">Signin</ion-button><ion-button @click="register">Create</ion-button>
+                </div>
+            </ion-list>
+        </div>
+        <div v-else>
+            <p>hello user</p>
+        </div>
+        </ion-content>
+  </ion-modal>
 </template>
 
 <script lang="ts" setup>
-
 import {
   IonButtons,
   IonIcon,
@@ -27,11 +54,33 @@ import {
   IonButton,
   IonItem,
   IonInput,
-} from '@ionic/vue';
-import { arrowBackCircleOutline } from 'ionicons/icons';
-import { OverlayEventDetail } from '@ionic/core/components';
-import { ref } from 'vue';
+  IonList,
+} from "@ionic/vue";
+import { arrowBackCircleOutline } from "ionicons/icons";
+import { onMounted, ref } from "vue";
+import {createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged} from "firebase/auth"
+import {auth} from "../utils/firebase"
+const text = ref('')
+const pass = ref('')
+async function register(){
+    try{
+        const data = await createUserWithEmailAndPassword(auth, text.value,pass.value)
+        console.log(`succesfull: ${data}`)
+    }catch (e){
+        console.log(e)
+    }
+}
+async function login(){
+    console.log("login")
+    try{
+        const data = await signInWithEmailAndPassword(auth, text.value,pass.value)
+        console.log(`succesfull: ${data}`)
+    }catch (e){
+        console.log(e)
+    }
+}
 const props = defineProps({
-  isOpen : Boolean
+  isOpen: Boolean,
+  isLoggedIn: Boolean
 });
 </script>
