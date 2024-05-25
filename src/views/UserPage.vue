87d6c419de2b69733ce0c449b68fabf674dcae40
NikoJunttila <89527972+NikoJunttila@users.noreply.github.com>
2024-05-25 10:05:00 +0300

    added chat bot

---
+++ src/views/UserPage.vue
@@ -0,0 +1,28 @@
+<template>
+  <ion-page>
+    <ion-content>
+      <h1>admin stuff</h1>
+      {{ JSON.stringify(userStore.user) }}
+      <ion-button @click="logout">Logout</ion-button>
+
+    </ion-content>
+  </ion-page>
+</template>
+
+<script setup lang="ts">
+import { IonPage, IonContent,IonButton } from "@ionic/vue";
+import { useUserStore } from "../store/userStore";
+import { signOut } from "firebase/auth";
+import { auth } from "../utils/firebase";
+
+const userStore = useUserStore();
+function logout() {
+  signOut(auth)
+    .then(() => {
+      console.log("logged out");
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}
+</script>
